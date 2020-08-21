85d6c80d734146136135d90fa169344a2e946a29
Carolina Fernández <carolinasolfernandez@gmail.com>
2020-08-21 04:40:55 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,6 +1,4 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import SimpleImputer
-from sklearn.impute import SimpleImputer
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -15,22 +13,3 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
-
-# Custom replace missing values
-class ReplaceMissingValues(BaseEstimator, TransformerMixin):
-    def __init__(self, mean_columns, zero_columns):
-        self.mean_columns = mean_columns
-        self.zero_columns = zero_columns
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        data = X.copy()
-        
-        for mean_column in self.mean_columns:
-          data[[mean_column]] = data[[mean_column]].fillna(data[[mean_column]].mean())
-        for zero_column in self.zero_columns:
-          data[[zero_column]] = data[[zero_column]].fillna(0)
-
-        return data
