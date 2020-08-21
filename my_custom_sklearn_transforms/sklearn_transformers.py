e3ede95a6d2d455cefdf5e742e07e43a7a0cf437
Carolina Fernández <carolinasolfernandez@gmail.com>
2020-08-21 02:16:59 -0300

    custom missing values

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,25 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+# Custom replace missing values
+class ReplaceMissingValues(BaseEstimator, TransformerMixin):
+    def __init__(self, mean_columns, zero_columns):
+        self.mean_columns = mean_columns
+        self.zero_columns = zero_columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+
+        mean_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+        zero_imputer = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=0)
+
+        for mean_column in self.mean_columns:
+          data[[mean_column]] = mean_imputer.fit_transform(data[[mean_column]])
+        for zero_column in self.zero_columns:
+          data[[zero_column]] = zero_imputer.fit_transform(data[[zero_column]])
+
+        return data
